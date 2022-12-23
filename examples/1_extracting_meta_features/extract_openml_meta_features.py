549868bc14bb620b9a30330f43b875d9b07130e7
morrisnein <petroochcho@gmail.com>
2022-12-23 17:51:52 +0300

    fix an example

--- examples/1_extracting_meta_features/extract_openml_meta_features.py
+++ examples/1_extracting_meta_features/extract_openml_meta_features.py
@@ -5,7 +5,7 @@ def main():
     dataset_names = [
         'nomao', 'sylvine', 'kc1', 'jungle_chess_2pcs_raw_endgame_complete', 'credit-g', 'delta_ailerons', 'pol'
     ]
-    extractor = OpenMLFeaturesExtractor().fit()
+    extractor = OpenMLFeaturesExtractor()
     meta_features = extractor.extract(dataset_names)
     return meta_features
 
