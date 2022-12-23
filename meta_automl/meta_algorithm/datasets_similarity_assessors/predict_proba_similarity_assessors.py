f0ac039019270a3483798bd5d7ab91c4ec1ec2fa
morrisnein <petroochcho@gmail.com>
2022-12-23 12:50:15 +0300

    edit `PredictProbaSimilarityAssessor`

--- meta_automl/meta_algorithm/datasets_similarity_assessors/predict_proba_similarity_assessors.py
+++ meta_automl/meta_algorithm/datasets_similarity_assessors/predict_proba_similarity_assessors.py
@@ -13,27 +13,38 @@ class PredictProbaSimilarityAssessor(DatasetsSimilarityAssessor):
         self._inner_model = model
         self.n_best = n_best
 
+    @property
+    def classes(self) -> List[str]:
+        return self._inner_model.classes_
+
+    @property
+    def feature_names(self) -> List[str]:
+        return self._inner_model.feature_names_in_
+
     @staticmethod
     def preprocess_meta_features(meta_features: pd.DataFrame) -> pd.DataFrame:
         return meta_features.dropna(axis=1, how='any')
 
-    def _process_predict_features(self, meta_features: pd.DataFrame) -> pd.DataFrame:
-        return meta_features[self._inner_model.feature_names_in_]
+    def _preprocess_predict_features(self, meta_features: pd.DataFrame) -> pd.DataFrame:
+        return meta_features[self.feature_names]
 
     def fit(self, meta_features: pd.DataFrame, datasets: Iterable[str]):
         meta_features = self.preprocess_meta_features(meta_features)
         self._inner_model.fit(meta_features, datasets)
 
+    def predict_proba(self, meta_features: pd.DataFrame) -> List[List[float]]:
+        return self._inner_model.predict_proba(meta_features)
+
     def predict(self, meta_features: pd.DataFrame) -> List[List[str]]:
-        meta_features = self._process_predict_features(meta_features)
-        predict_probs = self._inner_model.predict_proba(meta_features)
+        meta_features = self._preprocess_predict_features(meta_features)
+        predict_probs = self.predict_proba(meta_features)
         final_prediction = []
         for probabilities in predict_probs:
             probabilities = list(probabilities)
             predictions = []
             for _ in range(self.n_best):
                 predicted_class_idx = np.argmax(probabilities)
-                predicted_class = self._inner_model.classes_[predicted_class_idx]
+                predicted_class = self.classes[predicted_class_idx]
                 predictions.append(predicted_class)
                 probabilities.pop(predicted_class_idx)
             final_prediction.append(predictions)
