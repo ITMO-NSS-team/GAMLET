5ddf3633058f1c54bd71d6c844d3171f0f5da0b3
morrisnein <petroochcho@gmail.com>
2023-05-02 14:30:55 +0300

    don't download cached datasets qualities

--- meta_automl/data_preparation/datasets_loaders/openml_datasets_loader.py
+++ meta_automl/data_preparation/datasets_loaders/openml_datasets_loader.py
@@ -30,7 +30,7 @@ class OpenMLDatasetsLoader(DatasetsLoader):
         return self.get_openml_dataset(source)
 
     def get_openml_dataset(self, dataset_id: OpenMLDatasetID, force_download: bool = False) -> DatasetCache:
-        openml_dataset = openml.datasets.get_dataset(dataset_id, download_data=False)
+        openml_dataset = openml.datasets.get_dataset(dataset_id, download_data=False, download_qualities=False)
         name = openml_dataset.name.lower()
         dataset_cache_path = self.data_manager.get_dataset_cache_path(name)
         if dataset_cache_path.exists() and not force_download:
