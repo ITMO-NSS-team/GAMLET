a5b0e3c69c0059ffd0915b243d2f24ac02003447
morrisnein <petroochcho@gmail.com>
2023-07-06 18:07:27 +0300

    fix inner components

--- meta_automl/data_preparation/datasets_loaders/openml_datasets_loader.py
+++ meta_automl/data_preparation/datasets_loaders/openml_datasets_loader.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Union, Optional
+from typing import List, Union, Optional, Sequence
 
 from golem.core.log import default_log
 
@@ -10,18 +10,17 @@ from meta_automl.data_preparation.datasets_loaders import DatasetsLoader
 
 class OpenMLDatasetsLoader(DatasetsLoader):
     def __init__(self, allow_names: bool = False):
-        self.dataset_ids = []
+        self.dataset_ids = set()
         self._allow_names = allow_names
 
-    def load(self, dataset_ids: List[Union[OpenMLDatasetIDType, str]],
+    def load(self, dataset_ids: Sequence[Union[OpenMLDatasetIDType, str]],
              allow_names: Optional[bool] = None) -> List[OpenMLDataset]:
-        self.dataset_ids += dataset_ids
         allow_names = self._allow_names if allow_names is None else allow_names
 
         datasets = []
         # TODO: Optimize like this
         #  https://github.com/openml/automlbenchmark/commit/a09dc8aee96178dd14837d9e1cd519d1ec63f804
-        for dataset_id in self.dataset_ids:
+        for dataset_id in dataset_ids:
             dataset = self.load_single(dataset_id, allow_name=allow_names)
             datasets.append(dataset)
         return datasets
@@ -35,7 +34,8 @@ class OpenMLDatasetsLoader(DatasetsLoader):
         else:
             dataset = OpenMLDataset(dataset_id)
 
-        self.dataset_ids.append(dataset.id_)
+        self.dataset_ids.add(dataset.id_)
+
         return dataset
 
     @property
