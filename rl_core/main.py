0b24046ef906cfaacd05ff93d14b4d47f4d03f36
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2023-08-02 19:36:47 +0300

    Adding generator, agent and fixes in env (repeated)

--- rl_core/main.py
+++ rl_core/main.py
@@ -6,7 +6,7 @@ from rl_core.generator import Generator
 if __name__ == '__main__':
     task_type = 'classification'
     pipeline_len = 5
-    n_episodes = 5000
+    n_episodes = 300
 
     scoring_train_path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
     airlines_train_path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/airlines.csv')
@@ -19,9 +19,16 @@ if __name__ == '__main__':
     gen = Generator(task_type, state_dim=pipeline_len, n_episodes=n_episodes) \
         .set_environment(env_name='linear') \
         .set_dataloader(train_datasets) \
-        .set_agent()
+        .set_agent(eval_schedule=50)
 
     gen.fit()
 
-    print('A')
+    scoring_test_path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_test.csv')
+
+    # TODO: Adding Saving / Loading pre-trained agent
+
+    for _ in range(25):
+        pipeline, metric_value = gen.generate(path_to_dataset=scoring_test_path)
+        print(metric_value)
+        pipeline.show()
 
