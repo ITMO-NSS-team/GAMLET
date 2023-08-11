308ef8076133528e0c4d47e2a58c0bb3c93a572f
Egor Shikov <egorshikov@itmo.ru>
2023-08-11 13:05:05 +0000

    fixes for DataPipelineSurrogate and examples

--- meta_automl/meta_algorithm/model_advisors/__init__.py
+++ meta_automl/meta_algorithm/model_advisors/__init__.py
@@ -3,3 +3,4 @@ from meta_automl.meta_algorithm.model_advisors.model_advisor import (
     SimpleSimilarityModelAdvisor
 )
 from meta_automl.meta_algorithm.model_advisors.diverse_fedot_pipeline_advisor import DiverseFEDOTPipelineAdvisor
+from meta_automl.meta_algorithm.model_advisors.surrogate_advisor import SurrogateGNNPipelineAdvisor
