ad0652629998e89043a937baddf72650340fe112
DRMPN <mr.gorden.smit@gmail.com>
2023-08-13 03:15:37 +0300

    added docstrings to DatasetsSimilarityAssessor

--- meta_automl/meta_algorithm/datasets_similarity_assessors/datasets_similarity_assessor.py
+++ meta_automl/meta_algorithm/datasets_similarity_assessors/datasets_similarity_assessor.py
@@ -1,9 +1,17 @@
 from abc import abstractmethod
-
 from typing import List
 
 
 class DatasetsSimilarityAssessor:
+    """The Datasets similarity assessor is designed to assess the similarity of datasets by meta-features.
+
+    At the knowledge acquisition stage, a table of meta-features of datasets is input.
+    At the knowledge application stage, the component accepts meta-signs of new datasets and converts them into lists of
+    similar datasets from the previously "memorized" ones.
+
+    Optionally, it can also return a measure of similarity (or distance) between the new datasets and the "memorized" ones.
+    """
+
     @abstractmethod
     def fit(self, *args, **kwargs):
         raise NotImplementedError()
