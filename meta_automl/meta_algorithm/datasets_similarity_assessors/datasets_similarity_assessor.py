6ee69bb82a26e08f82964973a5d4804c37c15e0b
DRMPN <mr.gorden.smit@gmail.com>
2023-08-21 11:05:59 +0300

    small patch

--- meta_automl/meta_algorithm/datasets_similarity_assessors/datasets_similarity_assessor.py
+++ meta_automl/meta_algorithm/datasets_similarity_assessors/datasets_similarity_assessor.py
@@ -6,8 +6,8 @@ class DatasetsSimilarityAssessor:
     """The Datasets similarity assessor is designed to assess the similarity of datasets by meta-features.
 
     At the knowledge acquisition stage, a table of meta-features of datasets is input.
-    At the knowledge application stage, the component accepts meta-signs of new datasets and discovers
-    lists of similar datasets from the previously "memorized" ones.
+    At the knowledge application stage, the component accepts meta-signs of new datasets and
+    discovers similar datasets from the previously "memorized" ones.
 
     Optionally, it can also return a measure of similarity (or distance) between
     the new datasets and the "memorized" ones.
