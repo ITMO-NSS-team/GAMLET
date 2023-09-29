01cbfb2baa0ce7cc8ebea96442d22aa7162d03b9
DRMPN <mr.gorden.smit@gmail.com>
2023-09-29 10:57:00 +0300

    patch pip E402

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,9 @@ import datetime
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
 from sphinx.builders.html import StandaloneHTMLBuilder
+
+sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
