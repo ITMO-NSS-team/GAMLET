d85e4f94778b5a6fe3880560433da57c6cf75723
KonstantinChernyak <chernyakonstantin@gmail.com>
2023-10-04 00:04:48 +0300

    Move data-related code to meta_automl

--- meta_automl/data_preparation/meta_features_extractors/__init__.py
+++ meta_automl/data_preparation/meta_features_extractors/__init__.py
@@ -1,2 +1,7 @@
 from .meta_features_extractor import MetaFeaturesExtractor
 from .pymfe_extractor import PymfeExtractor
+from .openml_dataset_meta_features_extractor import OpenMLDatasetMetaFeaturesExtractor
+
+__all__ = [
+    "OpenMLDatasetMetaFeaturesExtractor",
+]
