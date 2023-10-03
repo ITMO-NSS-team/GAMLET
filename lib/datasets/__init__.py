1b2f0690b398c2023b78dffd857a6f91004e5dfd
ChernyakKonstantin <chernyakonstantin@gmail.com>
2023-10-04 00:04:48 +0300

    Add code from research internship

---
+++ lib/datasets/__init__.py
@@ -0,0 +1,7 @@
+from .homogeneous_pipeline_dataset import HomogeneousPipelineDataset
+from .synthetic_pipeline_dataset import SyntheticPipelineDataset
+
+__all__ = [
+    "HomogeneousPipelineDataset",
+    "SyntheticPipelineDataset",
+]
