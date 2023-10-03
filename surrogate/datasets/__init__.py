b4c07347e6d3dcd65e4c20489f1e173c682baba9
ChernyakKonstantin <chernyakonstantin@gmail.com>
2023-10-04 00:04:48 +0300

    Organize training code

--- surrogate/datasets/__init__.py
+++ surrogate/datasets/__init__.py
@@ -1,7 +1,9 @@
 from .homogeneous_pipeline_dataset import HomogeneousPipelineDataset
 from .synthetic_pipeline_dataset import SyntheticPipelineDataset
+from .data import GraphDataset
 
 __all__ = [
     "HomogeneousPipelineDataset",
     "SyntheticPipelineDataset",
+    "GraphDataset",
 ]
---
+++ surrogate/encoders/__init__.py
@@ -0,0 +1,9 @@
+from .graph_transformer import GraphTransformer
+from .homogeneous_gcn import HomogeneousGCN
+from .mlp_dataset_encoder import MLPDatasetEncoder
+
+__all__ = [
+    'GraphTransformer',
+    'HomogeneousGCN',
+    'MLPDatasetEncoder'
+]
