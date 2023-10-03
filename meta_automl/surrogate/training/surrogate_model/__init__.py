e67949044478c0bae0e6ed5617acc4f3414c1245
Egor Shikov <egorshikov@itmo.ru>
2023-10-04 00:11:02 +0300

    Change dataset format

--- meta_automl/surrogate/training/surrogate_model/__init__.py
+++ meta_automl/surrogate/training/surrogate_model/__init__.py
@@ -1,9 +1,8 @@
-from .train_surrogate_model import train_surrogate_model, get_pipelines_dataset, test_ranking
+from .train_surrogate_model import train_surrogate_model, test_ranking
 from .tune_surrogate_model import tune_surrogate_model
 
 __all__ = [
     "train_surrogate_model",
     "tune_surrogate_model",
-    "get_pipelines_dataset",
     "test_ranking",
 ]
