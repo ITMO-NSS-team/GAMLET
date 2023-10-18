248017e018d410fbe09f615cae75ef8f5771faf7
valer1435 <valp998@gmail.com>
2023-10-18 11:10:55 +0300

    fix test

--- meta_automl/data_preparation/dataset/time_series_dataset.py
+++ meta_automl/data_preparation/dataset/time_series_dataset.py
@@ -8,11 +8,14 @@ from meta_automl.data_preparation.file_system import get_project_root
 
 
 class TimeSeriesDataset(DatasetBase):
-    path_to_knowledge_base = Path(get_project_root(), 'data', 'knowledge_base_time_series_0', 'datasets')
 
-    def __init__(self, id_: DatasetIDType, forecast_length: int = 1):
+    def __init__(self, id_: DatasetIDType, forecast_length: int = 1, custom_path=None):
         super().__init__(id_)
         self.forecast_length = forecast_length
+        if custom_path:
+            self.path_to_knowledge_base = Path(custom_path)
+        else:
+            self.path_to_knowledge_base = Path(get_project_root(), 'data', 'knowledge_base_time_series_0', 'datasets')
 
     def get_data(self) -> TimeSeriesData:
         path_to_series = Path(self.path_to_knowledge_base, self.id_, 'data.csv')
--- test/unit/datasets/test_ts_dataset.py
+++ test/unit/datasets/test_ts_dataset.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from meta_automl.data_preparation.dataset.dataset_base import TimeSeriesData
 from meta_automl.data_preparation.dataset.time_series_dataset import TimeSeriesDataset
+from meta_automl.data_preparation.file_system import get_project_root
 
 
 def test_ts_dataset_creation(timeseries_dataset_ids):
@@ -11,6 +14,8 @@ def test_ts_dataset_creation(timeseries_dataset_ids):
 
 def test_ts_dataset_data_loading(timeseries_dataset_ids):
     for dataset_id in timeseries_dataset_ids:
-        dataset = TimeSeriesDataset(dataset_id)
+        dataset = TimeSeriesDataset(dataset_id,
+                                    custom_path=Path(get_project_root(), 'test', 'data', 'cache', 'datasets',
+                                                     'custom_dataset'))
         dataset_data = dataset.get_data()
         assert isinstance(dataset_data, TimeSeriesData)
