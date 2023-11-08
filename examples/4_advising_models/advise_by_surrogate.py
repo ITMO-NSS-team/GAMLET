a8a1772620fb33d92e77a34360360c68b77f728f
morrisnein <petroochcho@gmail.com>
2023-11-08 20:46:22 +0300

    fix arbitrary path at advise_by_surrogate.py example

--- examples/4_advising_models/advise_by_surrogate.py
+++ examples/4_advising_models/advise_by_surrogate.py
@@ -1,10 +1,11 @@
 import os
 import pickle
+from pathlib import Path
 
 import yaml
 
 from meta_automl.data_preparation.datasets_loaders import OpenMLDatasetsLoader
-from meta_automl.data_preparation.file_system.file_system import get_configs_dir
+from meta_automl.data_preparation.file_system.file_system import get_configs_dir, get_project_root
 from meta_automl.meta_algorithm.model_advisors import SurrogateGNNPipelineAdvisor
 
 
@@ -15,9 +16,14 @@ def main():
     with open(get_configs_dir() / 'run_surrogate_model.yml') as f:
         config = yaml.load(f, yaml.Loader)
 
-    with open(os.path.join(config["dataset_params"]["root_path"], "pipelines.pickle"), "rb") as input_file:
+    project_root = get_project_root()
+    pipelines_data_path = Path(config["dataset_params"]["root_path"], "pipelines.pickle")
+    pipelines_fedot_data_path = os.path.join(config["dataset_params"]["root_path"], "pipelines_fedot.pickle")
+    pipelines_data_path = project_root / pipelines_data_path
+    pipelines_fedot_data_path = project_root / pipelines_fedot_data_path
+    with open(pipelines_data_path, "rb") as input_file:
         pipelines_data = pickle.load(input_file)
-    with open(os.path.join(config["dataset_params"]["root_path"], "pipelines_fedot.pickle"), "rb") as input_file:
+    with open(pipelines_fedot_data_path, "rb") as input_file:
         pipelines_fedot = pickle.load(input_file)
 
     advisor = SurrogateGNNPipelineAdvisor(config, pipelines_data, pipelines_fedot)
