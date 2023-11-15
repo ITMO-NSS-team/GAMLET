c4a3cb6ca6a3e88d94c6b2852bfcd7535e0638bf
Konstantin.Chernyak <Konstantin.Chernyak@atom.team>
2023-11-15 20:54:05 +0300

    Add missing file

--- meta_automl/data_preparation/pipeline_features_extractors/__init__.py
+++ meta_automl/data_preparation/pipeline_features_extractors/__init__.py
@@ -1,5 +1,6 @@
 from .fedot_pipeline_features_extractor import FEDOTPipelineFeaturesExtractor
-
+from .joined_autoencoder import JoinedAutoencoderHyperparametersEmbedder
 __all__ = [
     "FEDOTPipelineFeaturesExtractor",
+    "JoinedAutoencoderHyperparametersEmbedder",
 ]
