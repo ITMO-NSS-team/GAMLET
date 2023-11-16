0d2e9214fec285552dd218eb55a7fa7d35683840
morrisnein <petroochcho@gmail.com>
2023-11-16 16:22:36 +0300

    remove meta-features cache, refactor mf extractor interface

--- meta_automl/data_preparation/feature_preprocessors/feature_preprocessors.py
+++ meta_automl/data_preparation/feature_preprocessors/feature_preprocessors.py
@@ -7,6 +7,24 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
+def explode_ungrouped_metafeatures(df: pd.DataFrame, n_features: int) -> pd.DataFrame:
+    output = []
+    for index, row in df.iterrows():
+        df_row = pd.DataFrame(row)
+        df_row['value'] = df_row['value'].apply(
+            (lambda value: value if isinstance(value, np.ndarray) else [value] * n_features))
+        df_row = df_row.drop(
+            df_row[(df_row['value'].apply(len) > n_features) | (df_row['value'].apply(len) == 0)].index)
+        df_row['value'] = df_row['value'].apply(
+            (lambda value: value if len(value) == n_features else [value[0]] * n_features))
+        df_row = df_row.explode('value')
+        df_row['feature'] = df_row.groupby('variable').cumcount()
+        df_row['dataset'] = index
+        df_row = df_row[['dataset', 'variable', 'feature', 'value']]
+        output.append(df_row)
+    return pd.concat(output)
+
+
 class FeaturesPreprocessor:
     """Wrapper over features preprocessors.
 
@@ -56,7 +74,9 @@ class FeaturesPreprocessor:
             single: bool = False,
     ) -> Dict[str, Union[float, np.ndarray]]:
         result = data.copy()
+
         if self.is_sum_none:
+            result = explode_ungrouped_metafeatures(result, len(self.features))
             for key in self.features:
                 data_ = data.loc[(data["feature"] == key), "value"].values.reshape(-1, 1)
                 result.loc[(data["feature"] == key), "value"] = self.preprocessors[key].transform(data_)
--- test/unit/test_meta_features_extractors.py
+++ test/unit/test_meta_features_extractors.py
@@ -3,13 +3,13 @@ from pathlib import Path
 
 import pytest
 
-from meta_automl.data_preparation.dataset import OpenMLDataset, TimeSeriesDataset
-from meta_automl.data_preparation.file_system import (get_dataset_cache_path_by_id, get_meta_features_cache_path,
-                                                      get_project_root)
+from meta_automl.data_preparation.dataset import OpenMLDataset
+from meta_automl.data_preparation.datasets_loaders import TimeSeriesDatasetsLoader
+from meta_automl.data_preparation.file_system import get_dataset_cache_path_by_id, get_project_root
 from meta_automl.data_preparation.meta_features_extractors import PymfeExtractor, TimeSeriesFeaturesExtractor
 from test.constants import (DATASETS_WITH_CACHED_META_FEATURES, OPENML_CACHED_DATASETS, OPENML_DATASET_IDS_TO_LOAD,
-                            TS_DATASETS_IDS_TO_LOAD, TS_DATASETS_WITH_CACHED_META_FEATURES)
-from test.unit.datasets.general_checks import assert_cache_file_exists, assert_file_unmodified_during_test
+                            TS_DATASETS_IDS_TO_LOAD)
+from test.unit.datasets.general_checks import assert_cache_file_exists
 
 
 @pytest.fixture
@@ -20,9 +20,6 @@ def dataset_ids():
         if dataset_id not in OPENML_CACHED_DATASETS:
             dataset_cache_path = get_dataset_cache_path_by_id(OpenMLDataset, dataset_id)
             shutil.rmtree(dataset_cache_path.parent, ignore_errors=True)
-        if dataset_id not in DATASETS_WITH_CACHED_META_FEATURES:
-            mf_cache_path = get_meta_features_cache_path(PymfeExtractor, OpenMLDataset, dataset_id)
-            mf_cache_path.unlink(missing_ok=True)
 
 
 @pytest.fixture
@@ -33,35 +30,19 @@ def timeseries_dataset_ids():
 
 def test_table_meta_features_extraction(dataset_ids):
     extractor = PymfeExtractor(extractor_params={'groups': 'general'})
-    meta_features = extractor.extract(dataset_ids, fill_input_nans=True)
+    datasets = [OpenMLDataset(dataset_id) for dataset_id in dataset_ids]
+    meta_features = extractor.extract(datasets, fill_input_nans=True)
     assert list(meta_features.index) == dataset_ids
     for dataset_id in dataset_ids:
-        meta_features_cache_path = get_meta_features_cache_path(PymfeExtractor, OpenMLDataset, dataset_id)
         dataset_cache_path = get_dataset_cache_path_by_id(OpenMLDataset, dataset_id)
-
-        assert_cache_file_exists(meta_features_cache_path)  # Meta-features were cached
-
-        if dataset_id in DATASETS_WITH_CACHED_META_FEATURES:
-            assert_file_unmodified_during_test(meta_features_cache_path)  # Test data were not modified
-        else:
-            assert_cache_file_exists(dataset_cache_path)  # Extractor downloaded necessary data
-
-        if dataset_id in set(DATASETS_WITH_CACHED_META_FEATURES) - set(OPENML_CACHED_DATASETS):
-            assert not dataset_cache_path.exists()  # Extractor did not download unnecessary data
+        assert_cache_file_exists(dataset_cache_path)  # Extractor downloaded necessary data
 
 
 def test_ts_meta_features_extraction(timeseries_dataset_ids):
-    extractor = TimeSeriesFeaturesExtractor(custom_path=Path(get_project_root(), 'test', 'data', 'cache', 'datasets',
-                                                             'custom_dataset'))
-    meta_features = extractor.extract(timeseries_dataset_ids, use_cached=False, update_cached=False)
+    datasets_loader = TimeSeriesDatasetsLoader(
+        custom_path=Path(get_project_root(),
+                         'test', 'data', 'cache', 'datasets', 'custom_dataset'))
+    datasets = datasets_loader.load(dataset_ids=timeseries_dataset_ids)
+    extractor = TimeSeriesFeaturesExtractor()
+    meta_features = extractor.extract(datasets)
     assert list(meta_features.index) == timeseries_dataset_ids
-    for dataset_id in timeseries_dataset_ids:
-        meta_features_cache_path = get_meta_features_cache_path(TimeSeriesFeaturesExtractor, TimeSeriesDataset,
-                                                                dataset_id)
-        assert_cache_file_exists(meta_features_cache_path)
-
-        if dataset_id in TS_DATASETS_WITH_CACHED_META_FEATURES:
-            assert_file_unmodified_during_test(meta_features_cache_path)
-        else:
-            cache_path = get_dataset_cache_path_by_id(TimeSeriesDataset, dataset_id)
-            assert_cache_file_exists(cache_path)
