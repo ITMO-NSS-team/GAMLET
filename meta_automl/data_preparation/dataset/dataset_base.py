bb153b6ec57f61caa6748094f45ed71e930ca42f
morrisnein <petroochcho@gmail.com>
2023-11-16 16:11:23 +0300

    introduce generic typing & index retrieval for dataset data

--- meta_automl/data_preparation/dataset/dataset_base.py
+++ meta_automl/data_preparation/dataset/dataset_base.py
@@ -1,44 +1,62 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from copy import copy
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, List, Optional
+from typing import Generic, Hashable, List, Optional, TypeVar
 
 import numpy as np
 
 from meta_automl.data_preparation.file_system import CacheOperator, get_dataset_cache_path
 
-DatasetIDType = Any
-
 
 @dataclass
 class DatasetData:
+    dataset: DatasetBase
     x: np.array
     y: Optional[np.array] = None
+
+    @property
+    def id(self):
+        return self.dataset.id
+
+    def __getitem__(self, item):
+        other = copy(self)
+        if self.y is not None:
+            other.y = self.y[item]
+        other.x = self.x[item]
+        return other
+
+
+@dataclass
+class TabularData(DatasetData):
     categorical_indicator: Optional[List[bool]] = None
     attribute_names: Optional[List[str]] = None
 
 
 @dataclass
-class TimeSeriesData:
-    x: np.array
+class TimeSeriesData(DatasetData):
     # time series has already split
-    y: np.array
     forecast_length: int = 1
 
 
-class DatasetBase(ABC, CacheOperator):
+DatasetDataType_co = TypeVar('DatasetDataType_co', bound=DatasetData, covariant=True)
+
+DatasetIDType = TypeVar('DatasetIDType', bound=Hashable)
+
+
+class DatasetBase(Generic[DatasetDataType_co], CacheOperator, ABC):
 
     def __init__(self, id_: DatasetIDType, name: Optional[str] = None):
-        self.id_ = id_
+        self.id = id_
         self.name = name
 
     def __repr__(self):
-        return f'{self.__class__.__name__}(id_={self.id_}, name={self.name})'
+        return f'{self.__class__.__name__}(id={self.id}, name={self.name})'
 
     @abstractmethod
-    def get_data(self) -> DatasetData:
+    def get_data(self) -> DatasetDataType_co:
         raise NotImplementedError()
 
     @property
--- meta_automl/data_preparation/datasets_loaders/datasets_loader.py
+++ meta_automl/data_preparation/datasets_loaders/datasets_loader.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import List, Sequence
+from typing import Generic, List, Sequence, TypeVar
 
 from meta_automl.data_preparation.dataset import DatasetBase, DatasetIDType
 
+DatasetType_co = TypeVar('DatasetType_co', bound=DatasetBase, covariant=True)
 
-class DatasetsLoader(ABC):
 
-    def load(self, dataset_ids: Sequence[DatasetIDType]) -> List[DatasetBase]:
+class DatasetsLoader(Generic[DatasetType_co], ABC):
+
+    def load(self, dataset_ids: Sequence[DatasetIDType]) -> List[DatasetType_co]:
         datasets = []
         for dataset_id in dataset_ids:
             dataset = self.load_single(dataset_id)
@@ -16,7 +18,7 @@ class DatasetsLoader(ABC):
         return datasets
 
     @abstractmethod
-    def load_single(self, *args, **kwargs) -> DatasetBase:
+    def load_single(self, *args, **kwargs) -> DatasetType_co:
         raise NotImplementedError()
 
     @property
