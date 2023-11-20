65ce485e03545ca665c37c04b769fcf6e3590817
Konstantin.Chernyak <Konstantin.Chernyak@atom.team>
2023-11-20 22:35:15 +0300

    Add first version of training surrogate with hyperparameters

--- meta_automl/surrogate/training/__init__.py
+++ meta_automl/surrogate/training/__init__.py
@@ -1,8 +1,10 @@
 from .train_surrogate_model import train_surrogate_model, test_ranking
 from .tune_surrogate_model import tune_surrogate_model
+from .train_surrogate_model_new import train_surrogate_model_new
 
 __all__ = [
     "train_surrogate_model",
     "tune_surrogate_model",
     "test_ranking",
+    "train_surrogate_model_new",
 ]
