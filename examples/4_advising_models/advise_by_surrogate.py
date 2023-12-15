2dba7d35e41ee0fa586379ead1f6a249eba06494
Konstantin Chernyak <48140068+ChernyakKonstantin@users.noreply.github.com>
2023-12-15 15:21:05 +0300

    Issue 73 (#80)
    * Update advisor name
    
    * Turn feature extractor and preprocessor into arguments
    
    * Move pipelines to predict method args
    
    * Add use of precomputed dataset features
    
    * Turn surrogate model into initialization argument
    
    * Rafactor _predict_single method
    
    * Apply isort and black
    
    * Update advise_by_surrogate example
    
    * Mitigate rebase issues
    
    * fix data paths
    
    * fix datasets loading
    
    * Move feature extraction out of SurrogateGNNModelAdvisor
    
    * adapt the example advise_by_surrogate.py
    
    ---------
    
    Co-authored-by: Konstantin.Chernyak <Konstantin.Chernyak@atom.team>
    Co-authored-by: morrisnein <petroochcho@gmail.com>

--- examples/4_advising_models/advise_by_surrogate.py
+++ examples/4_advising_models/advise_by_surrogate.py
@@ -1,34 +1,92 @@
-import os
+""" Example on advising pipelines for a dataset. """
+
 import pickle
-from pathlib import Path
 
-import yaml
+import pandas as pd
+import torch
+from torch_geometric.data import Data
 
-from meta_automl.data_preparation.datasets_loaders import OpenMLDatasetsLoader
-from meta_automl.data_preparation.file_system.file_system import get_configs_dir, get_project_root
-from meta_automl.meta_algorithm.model_advisors import SurrogateGNNPipelineAdvisor
+from meta_automl.data_preparation.dataset import DatasetBase, OpenMLDataset
+from meta_automl.data_preparation.feature_preprocessors import FeaturesPreprocessor
+from meta_automl.data_preparation.file_system.file_system import get_checkpoints_dir, get_configs_dir, get_data_dir
+from meta_automl.data_preparation.meta_features_extractors import MetaFeaturesExtractor, PymfeExtractor
+from meta_automl.data_preparation.pipeline_features_extractors import FEDOTPipelineFeaturesExtractor
+from meta_automl.meta_algorithm.model_advisors import SurrogateGNNModelAdvisor
+from meta_automl.surrogate.data_pipeline_surrogate import get_extractor_params
+from meta_automl.surrogate.surrogate_model import RankingPipelineDatasetSurrogateModel
+
+SURROGATE_MODEL_CHECKPOINT_FILE = get_checkpoints_dir() / "tabular" / "checkpoints" / "best.ckpt"
+SURROGATE_MODEL_HYPERPARAMETERS_FILE = get_checkpoints_dir() / "tabular" / "hparams.yaml"
+META_FEATURES_EXTRACTOR_CONFIG_FILE = get_configs_dir() / "use_features.json"
+META_FEATURES_PREPROCESSOR_FILE = (
+        get_data_dir() / "pymfe_meta_features_and_fedot_pipelines/all/meta_features_preprocessors.pickle"
+)
+PIPELINES_FILE = get_data_dir() / "pymfe_meta_features_and_fedot_pipelines/all/pipelines_fedot.pickle"
+
+
+def preprocess_dataset_features(
+        dataset: DatasetBase,
+        dataset_meta_features_extractor: MetaFeaturesExtractor,
+        dataset_meta_features_preprocessor: FeaturesPreprocessor,
+) -> Data:
+    """Extract dataset features.
+
+    Parameters
+    ----------
+    dataset: Dataset to extract features from.
+
+    dataset_meta_features_extractor: Extractor returning dataset meta-features.
+
+    dataset_meta_features_preprocessor: Preprocessor, preparing the meta-features after extraction.
+
+    Returns
+    -------
+    dset_data : Data
+        Dataset features.
+
+    """
+    x = dataset_meta_features_extractor.extract([dataset], fill_input_nans=True, use_cached=False).fillna(0)
+    x = dataset_meta_features_preprocessor.transform(x, single=False).fillna(0)
+    transformed = x.groupby(by=["dataset", "variable"])["value"].apply(list).apply(lambda x: pd.Series(x))
+    dset_data = Data(x=torch.tensor(transformed.values, dtype=torch.float32))
+    return dset_data
 
 
 def main():
-    # Define datasets
-    dataset = OpenMLDatasetsLoader().load(["apsfailure"], allow_names=True)
-    # Extract meta-features and load on demand.
-    with open(get_configs_dir() / 'run_surrogate_model.yml') as f:
-        config = yaml.load(f, yaml.Loader)
+    # Build model to estimate pipelines.
+    surrogate_model = RankingPipelineDatasetSurrogateModel.load_from_checkpoint(
+        checkpoint_path=SURROGATE_MODEL_CHECKPOINT_FILE,
+        hparams_file=SURROGATE_MODEL_HYPERPARAMETERS_FILE,
+    )
+    # Build dataset meta features extractor.
+    extractor_params = get_extractor_params(META_FEATURES_EXTRACTOR_CONFIG_FILE)
+    dataset_meta_features_extractor = PymfeExtractor(**extractor_params)
+    # Build dataset meta features preprocessor.
+    dataset_meta_features_preprocessor = FeaturesPreprocessor(load_path=META_FEATURES_PREPROCESSOR_FILE)
+    # Build pipeline features extractor.
+    pipeline_extractor = FEDOTPipelineFeaturesExtractor(
+        include_operations_hyperparameters=False,
+        operation_encoding="ordinal",
+    )
+    # Build adviser.
+    advisor = SurrogateGNNModelAdvisor(surrogate_model)
+    # Load datasets.
+    datasets_names = [
+        "apsfailure",
+    ]
+    datasets = [OpenMLDataset.from_search(dset_name) for dset_name in datasets_names]
+    # Load pipelines.
+    with open(PIPELINES_FILE, "rb") as input_file:
+        pipelines = pickle.load(input_file)
 
-    project_root = get_project_root()
-    pipelines_data_path = Path(config["dataset_params"]["root_path"], "pipelines.pickle")
-    pipelines_fedot_data_path = os.path.join(config["dataset_params"]["root_path"], "pipelines_fedot.pickle")
-    pipelines_data_path = project_root / pipelines_data_path
-    pipelines_fedot_data_path = project_root / pipelines_fedot_data_path
-    with open(pipelines_data_path, "rb") as input_file:
-        pipelines_data = pickle.load(input_file)
-    with open(pipelines_fedot_data_path, "rb") as input_file:
-        pipelines_fedot = pickle.load(input_file)
+    # Extract features
+    pipelines_features = [pipeline_extractor(pipeline.save()[0]) for pipeline in pipelines]
+    datasets_features = [
+        preprocess_dataset_features(dataset, dataset_meta_features_extractor, dataset_meta_features_preprocessor) for
+        dataset in datasets]
 
-    advisor = SurrogateGNNPipelineAdvisor(config, pipelines_data, pipelines_fedot)
-
-    return advisor.predict(dataset)
+    # Make prediction.
+    return advisor.predict(pipelines, datasets, pipelines_features, datasets_features, k=5)
 
 
 if __name__ == "__main__":
--- meta_automl/meta_algorithm/model_advisors/surrogate_advisor.py
+++ meta_automl/meta_algorithm/model_advisors/surrogate_advisor.py
@@ -1,104 +1,111 @@
-from typing import Any, Dict, List
+from typing import List
 
 import numpy as np
-import pandas as pd
 import torch
+import torch.nn as nn
+from fedot.core.pipelines.pipeline import Pipeline
 from golem.core.optimisers.fitness import SingleObjFitness
-from torch_geometric.data import Data
-from torch_geometric.loader import DataLoader
+from torch_geometric.data import Batch, Data
 
 from meta_automl.data_preparation.dataset import DatasetBase
 from meta_automl.data_preparation.evaluated_model import EvaluatedModel
-from meta_automl.data_preparation.feature_preprocessors import FeaturesPreprocessor
-from meta_automl.data_preparation.file_system import get_data_dir
-from meta_automl.data_preparation.file_system.file_system import get_checkpoints_dir, get_configs_dir
-from meta_automl.data_preparation.meta_features_extractors import PymfeExtractor
 from meta_automl.meta_algorithm.model_advisors import ModelAdvisor
-from meta_automl.surrogate.data_pipeline_surrogate import get_extractor_params
-from meta_automl.surrogate.surrogate_model import RankingPipelineDatasetSurrogateModel
 
 
-class SurrogateGNNPipelineAdvisor(ModelAdvisor):
-    """Pipeline advisor based on surrogate GNN model.
+class SurrogateGNNModelAdvisor(ModelAdvisor):
+    """ModelAdvisor based on dataset-and-pipeline-features-aware Graph Neural Network.
 
     Parameters:
     -----------
-    config:
-        Dict of model parameters. The parameters are: TODO.
+    surrogate_model: nn.Module
+        Surrogate model to be used.
     """
 
-    def __init__(self, config: Dict[str, Any], pipelines, pipelines_fedot):
-        pipelines = pipelines
-        self.pipelines_fedot = pipelines_fedot
-        self.pipeline_dataloader = DataLoader(pipelines, batch_size=1)
-
-        # loading surrogate model
-        checkpoints_dir = get_checkpoints_dir() / 'tabular'
-        self.surrogate_model = RankingPipelineDatasetSurrogateModel.load_from_checkpoint(
-            checkpoint_path=checkpoints_dir / 'checkpoints/best.ckpt',
-            hparams_file=checkpoints_dir / 'hparams.yaml'
-        )
+    def __init__(
+        self,
+        surrogate_model: nn.Module,
+    ):
+        self.surrogate_model = surrogate_model
         self.surrogate_model.eval()
+        self.device = next(self.surrogate_model.parameters()).device
 
-        # Prepare dataset extractor and extract metafeatures
-        config_dir = get_configs_dir() / 'use_features.json'
-        extractor_params = get_extractor_params(config_dir)
-        self.meta_features_extractor = PymfeExtractor(**extractor_params)
-        self.meta_features_preprocessor = FeaturesPreprocessor(
-            load_path=get_data_dir() / "pymfe_meta_features_and_fedot_pipelines/all/meta_features_preprocessors.pickle")
+    def _predict_single(
+        self,
+        dataset: DatasetBase,
+        dataset_features: Data,
+        pipelines: List[Pipeline],
+        pipelines_features: List[Data],
+        k: int,
+    ) -> List[EvaluatedModel]:
+        """Select optimal pipelines for given dataset.
 
-    def _preprocess_dataset_features(self, dataset):
-        x = self.meta_features_extractor.extract([dataset], fill_input_nans=True).fillna(0)
-        x = self.meta_features_preprocessor.transform(x, single=False).fillna(0)
-        transformed = x.groupby(by=['dataset', 'variable'])['value'].apply(list).apply(lambda x: pd.Series(x))
-        dset_data = Data(torch.tensor(transformed.values, dtype=torch.float32))
-        dset_data_loader = DataLoader([dset_data], batch_size=1)
-        return next(iter(dset_data_loader))
-
-    def _predict_single(self, dataset: DatasetBase, k) -> List[EvaluatedModel]:
-        """Predict optimal pipelines for given dataset.
         Parameters
         ----------
         dataset: DatasetBase
-            dataset
+            Dataset to select pipelines for.
+        dataset_features: Data
+            Dataset meta-features.
+        pipelines: List[Pipeline]
+            List of pipelines to select from.
+        pipelines_features: List[Data]
+            Extracted pipelines features to infer surrogate on.
         k: int, optional
-            number of pipelines to predict (default: 5)
+            Number of pipelines to predict (default: 5)
+
         Returns
         -------
         top_models : [Model]
             Top models for dataset.
+
         """
-        x_dset = self._preprocess_dataset_features(dataset)
+        dataset_features = Batch.from_data_list([dataset_features]).to(self.device)
 
         scores = []
         with torch.no_grad():
-            for batch in self.pipeline_dataloader:
-                scores.append(torch.squeeze(self.surrogate_model(batch, x_dset)).numpy())
+            for pipeline_features in pipelines_features:
+                pipeline_features = Batch.from_data_list([pipeline_features]).to(self.device)
+                score = self.surrogate_model(pipeline_features, dataset_features).cpu().item()
+                scores.append(score)
 
         scores = np.array(scores)
         indx = np.argsort(scores)
         k = min(len(indx), k)
         best_models = []
         for i in indx[-k:][::-1]:
-            best_models.append(
-                EvaluatedModel(
-                    self.pipelines_fedot[i],
-                    SingleObjFitness(scores[i]),
-                    'surrogate_fitness',
-                    dataset))
+            best_models.append(EvaluatedModel(pipelines[i], SingleObjFitness(scores[i]), "surrogate_fitness", dataset))
         return best_models
 
-    def predict(self, datasets: List[DatasetBase], k: int = 5) -> List[List[EvaluatedModel]]:
-        """Predict optimal pipelines for given list of datasets dataset.
+    def predict(
+        self,
+        pipelines: List[Pipeline],
+        datasets: List[DatasetBase],
+        pipelines_features: List[Data],
+        datasets_features: List[Data],
+        k: int = 5,
+    ) -> List[List[EvaluatedModel]]:
+        """Select optimal pipelines for given list of datasets.
+
         Parameters
         ----------
         datasets: List[DatasetBase]
-            list of DatasetBase objects
+            List of `DatasetBase` objects to select pipelines for.
+        pipelines: List[Pipeline]
+            List of pipelines to select from.
         k: int, optional
-            number of pipelines to predict (default: 5)
+            Number of pipelines to predict per dataset (default: 5).
+        pipelines_features: List[Data], optional
+            Extracted pipelines features to infer surrogate on (default: None).
+        datasets_features: List[Data], optional
+            Extracted datasets features to infer surrogate on (default: None).
+
         Returns
         -------
         top_models : List[List[EvaluatedModel]]
             List of top models for each dataset.
+
         """
-        return [self._predict_single(dset, k) for dset in datasets]
+
+        top_models = []
+        for dset, dset_feats in zip(datasets, datasets_features):
+            top_models.append(self._predict_single(dset, dset_feats, pipelines, pipelines_features, k))
+        return top_models
