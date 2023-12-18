c27534c0615455396a3cb30d40f32a186d20ceee
morrisnein <petroochcho@gmail.com>
2023-12-18 23:09:14 +0300

    smth

--- meta_automl/data_preparation/dataset/__init__.py
+++ meta_automl/data_preparation/dataset/__init__.py
@@ -1,4 +1,4 @@
 from .dataset_base import DatasetBase, DatasetData, DatasetIDType, TabularData, TimeSeriesData
 from .custom_dataset import CustomDataset, DataNotFoundError
 from .openml_dataset import OpenMLDataset, OpenMLDatasetIDType
-from .time_series_dataset import TimeSeriesDataset
+from .time_series_dataset import (TimeSeriesDataset)
