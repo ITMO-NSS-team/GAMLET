a04c49cdbd6314b91923176a2d6dd465e5c96e47
morrisnein <petroochcho@gmail.com>
2023-12-18 15:30:11 +0300

    add docstrings

--- meta_automl/data_preparation/model_fitness_scalers/dataset_models_fitness_scaler.py
+++ meta_automl/data_preparation/model_fitness_scalers/dataset_models_fitness_scaler.py
@@ -12,11 +12,18 @@ ScalerType = TypeVar('ScalerType', OneToOneFeatureMixin, TransformerMixin)
 
 
 class DatasetModelsFitnessScaler:
+    """ Uses a given scikit-learn scaler class to scale fitness scores of models between 0 and 1 per given datasets.
+    Args:
+        :scaler_class: The scaler class to use to scale fitness. Defaults to MinMaxScaler.
+    """
+
     def __init__(self, scaler_class: Type[ScalerType] = MinMaxScaler):
         self.scaler_class = scaler_class
         self.scalers: Dict[str, ScalerType] = {}
 
     def fit(self, models: Sequence[Sequence[EvaluatedModel]], datasets: Sequence[DatasetType_co]) -> Self:
+        """ Creates new instances of scaler per given datasets and stores them in a dictionary,
+            where key is the string representation of the dataset and the value is the fitted scaler instance. """
         dataset_representations = map(repr, datasets)
         for dataset_repr, dataset_models in zip(dataset_representations, models):
             scaler = self.scaler_class()
@@ -26,6 +33,7 @@ class DatasetModelsFitnessScaler:
         return self
 
     def transform(self, models: Sequence[Sequence[EvaluatedModel]], datasets: Sequence[DatasetType_co]):
+        """ Transforms fitness of the models given. """
         new_models = [[copy(model) for model in dataset_models] for dataset_models in models]
         dataset_representations = map(repr, datasets)
         for dataset_repr, dataset_models in zip(dataset_representations, new_models):
@@ -41,5 +49,6 @@ class DatasetModelsFitnessScaler:
     def fit_transform(self,
                       models: Sequence[Sequence[EvaluatedModel]],
                       datasets: Sequence[DatasetType_co]) -> Sequence[Sequence[EvaluatedModel]]:
+        """ Applies fit and transform to the models given. """
         self.fit(models, datasets)
         return self.transform(models, datasets)
