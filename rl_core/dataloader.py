1381df42365f2dca67d7fba965586386ecdbdf87
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2024-02-07 17:47:09 +0300

    Normilize meta data

--- rl_core/dataloader.py
+++ rl_core/dataloader.py
@@ -6,6 +6,7 @@ from fedot.core.data.data import InputData
 from fedot.core.data.data_split import train_test_data_setup
 from fedot.core.repository.dataset_types import DataTypesEnum
 from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
+from sklearn import preprocessing
 
 
 class DataLoader:
@@ -33,20 +34,22 @@ class DataLoader:
 class DataLoader_TS(DataLoader):
     def __init__(self, datasets: dict, forecast_length: dict = None, path_to_meta_data: str = None):
         super().__init__(datasets)
+        self.dataset_name = None
         self.forecast_length = None
         self.forecast_length_mapping = {'D': 14, 'W': 13, 'M': 18, 'Q': 8, 'Y': 6}
-        self.meta_data = pd.read_csv(path_to_meta_data, index_col=0).transpose() if path_to_meta_data else None
+        self.meta_data = pd.read_csv(path_to_meta_data, index_col=0) if path_to_meta_data else None
+        self.meta_data = self.normilize_data(self.meta_data).transpose()
 
     def get_data(self, dataset_name: str = None):
         if dataset_name is None:
-            dataset_name = random.choice(self.dataset_names)
+            self.dataset_name = random.choice(self.dataset_names)
 
-        path = self.datasets[dataset_name]
-        ts_type = dataset_name[3]
+        path = self.datasets[self.dataset_name]
+        ts_type = self.dataset_name[3]
         self.forecast_length = self.forecast_length_mapping[ts_type]
 
         if isinstance(self.meta_data, pd.DataFrame):
-            meta_data = self.meta_data[dataset_name].values
+            meta_data = self.meta_data[self.dataset_name].values
         else:
             meta_data = None
 
@@ -77,3 +80,9 @@ class DataLoader_TS(DataLoader):
         )
 
         return train_data, test_data, predict_input, meta_data
+
+    def normilize_data(self, data):
+        x = data.values  # returns a numpy array
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        return pd.DataFrame(x_scaled, index=data.index, columns=data.columns)
