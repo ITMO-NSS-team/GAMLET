Fix in dataloader, adding entropy regularisation and kl div

rl_core/agent/ppo.py
@@ -30,8 +30,9 @@ class Buffer:
 class PPO(nn.Module):
     metadata = {'name': 'PPO'}
 
-    def __init__(self, state_dim, action_dim, hidden_dim: int = 128, gamma: float = 0.95, batch_size: int = 128, epsilon: float = 0.3,
-                 pi_lr: float = 5e-5, v_lr: float = 5e-5, epoch_n: int = 30, device: str = 'cpu'):
+    def __init__(self, state_dim, action_dim, hidden_dim: int = 128, gamma: float = 0.9, batch_size: int = 128,
+                 epsilon: float = 0.2, tau: float = 1.0,
+                 pi_lr: float = 3e-4, v_lr: float = 3e-4, epoch_n: int = 30, device: str = 'cpu'):
         super().__init__()
 
         self.state_dim = state_dim
@@ -39,20 +40,23 @@ class PPO(nn.Module):
 
         self.pi_model = nn.Sequential(
             nn.Linear(state_dim, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim * 2), nn.ReLU(),
+            nn.Linear(hidden_dim * 2, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, action_dim),
             nn.Softmax(dim=-1)
         )
 
         self.v_model = nn.Sequential(
             nn.Linear(state_dim, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim * 2), nn.ReLU(),
+            nn.Linear(hidden_dim * 2, hidden_dim), nn.ReLU(),
             nn.Linear(hidden_dim, 1)
         )
 
         self.gamma = gamma
         self.batch_size = batch_size
         self.epsilon = epsilon
+        self.tau = tau
 
         self.pi_optimizer = torch.optim.Adam(self.pi_model.parameters(), lr=pi_lr)
         self.v_optimizer = torch.optim.Adam(self.v_model.parameters(), lr=v_lr)
@@ -106,12 +110,14 @@ class PPO(nn.Module):
                 b_pi_out = self.pi_model(b_states)
                 b_dist = Categorical(probs=b_pi_out)
                 b_new_log_probs = b_dist.log_prob(b_actions)
+                entropy = b_dist.entropy().mean()
+                entropy_penalty = - self.tau * entropy
 
                 b_ratio = torch.exp(b_new_log_probs - b_old_log_probs)
                 pi_loss_1 = b_ratio * b_advantage.detach()
                 pi_loss_2 = torch.clamp(b_ratio, 1 - self.epsilon, 1 + self.epsilon) * b_advantage.detach()
 
-                pi_loss = -torch.mean(torch.min(pi_loss_1, pi_loss_2))
+                pi_loss = -torch.mean(torch.min(pi_loss_1, pi_loss_2)) + entropy_penalty
                 pi_loss.backward()
                 self.pi_optimizer.step()
                 self.pi_optimizer.zero_grad()
@@ -121,7 +127,9 @@ class PPO(nn.Module):
                 self.v_optimizer.step()
                 self.v_optimizer.zero_grad()
 
-        return pi_loss, v_loss
+                kl_div = torch.mean((b_ratio - 1) - b_ratio).detach().cpu().numpy()
+
+        return pi_loss, v_loss, kl_div
 
     def append_to_buffer(self, s, a, r, terminated):
         self.buffer.append(s, a, r, terminated)

rl_core/dataloader.py
@@ -43,6 +43,8 @@ class DataLoader_TS(DataLoader):
     def get_data(self, dataset_name: str = None):
         if dataset_name is None:
             self.dataset_name = random.choice(self.dataset_names)
+        else:
+            self.dataset_name = dataset_name
 
         path = self.datasets[self.dataset_name]
         ts_type = self.dataset_name[3]
@@ -56,30 +58,31 @@ class DataLoader_TS(DataLoader):
         return self.load_data(path, meta_data)
 
     def load_data(self, path_to_dataset, meta_data):
-        # TODO: Rewrite getting train, test and pred_input. There are some mistakes in FEDOT
         task = Task(TaskTypesEnum.ts_forecasting, TsForecastingParams(self.forecast_length))
         data = pd.read_csv(path_to_dataset)['value'].values.reshape(-1, 1)
 
         X = data[:-self.forecast_length]
         y = data[-self.forecast_length:]
 
-        train_data = InputData(idx=np.arange(len(X)), features=X, target=X, task=task,
-                               data_type=DataTypesEnum.ts)
-
-        test_data = InputData(idx=np.arange(len(y)), features=y, target=y, task=task,
-                              data_type=DataTypesEnum.ts)
-
-        start_forecast = len(train_data.features)
-        end_forecast = start_forecast + self.forecast_length
-        predict_input = InputData(
-            idx=np.arange(start_forecast, end_forecast),
-            features=y,
-            target=None,
+        train_data = InputData(
+            idx=np.arange(len(X)),
+            features=np.ravel(X),
+            target=np.ravel(X),
             task=task,
             data_type=DataTypesEnum.ts
         )
 
-        return train_data, test_data, predict_input, meta_data
+        start_forecast = len(train_data.features)
+        end_forecast = start_forecast + self.forecast_length
+        test_data = InputData(
+            idx=np.arange(start_forecast, end_forecast),
+            features=np.ravel(X),
+            target=np.ravel(y),
+            task=task,
+            data_type=DataTypesEnum.ts
+        )
+
+        return train_data, test_data, meta_data
 
     def normilize_data(self, data):
         x = data.values  # returns a numpy array

rl_core/environments/time_series.py
@@ -2,6 +2,7 @@ import os
 from itertools import product, combinations
 from typing import Optional
 
+import matplotlib.pyplot as plt
 import gymnasium as gym
 import numpy as np
 import pygame
@@ -18,6 +19,8 @@ from sklearn.model_selection import train_test_split
 
 from meta_automl.utils import project_root
 from rl_core.dataloader import DataLoader, DataLoader_TS
+
+PLOT_PRED = False
 
 
 class TimeSeriesPipelineEnvironment(gym.Env):
@@ -90,7 +93,6 @@ class TimeSeriesPipelineEnvironment(gym.Env):
 
         self._train_data = None
         self._test_data = None
-        self._predict_input = None
         self._meta_data = None
 
         self.max_timestamp = max_timestamp  # TODO: Requires to automatize
@@ -217,8 +219,14 @@ class TimeSeriesPipelineEnvironment(gym.Env):
         try:
             self._pipeline.fit(self._train_data)
 
-            y_pred = self._pipeline.predict(self._predict_input).predict
+            y_pred = self._pipeline.predict(self._test_data).predict
             y_true = self._test_data.target
+
+            if PLOT_PRED:
+                plt.plot(range(0, len(self._train_data.target)), self._train_data.target)
+                plt.plot(range(len(self._train_data.target), len(self._train_data.target) + len(y_pred)), y_pred)
+                plt.plot(range(len(self._train_data.target), len(self._train_data.target) + len(y_true)), y_true)
+                plt.show()
 
             metric = mean_absolute_error(y_true, y_pred)
 
@@ -269,12 +277,10 @@ class TimeSeriesPipelineEnvironment(gym.Env):
             self,
             train_data: Optional[InputData],
             test_data: Optional[InputData],
-            predict_input: Optional[InputData],
             meta_data: Optional[np.ndarray]
     ):
         self._train_data = train_data
         self._test_data = test_data
-        self._predict_input = predict_input
         self._meta_data = meta_data
 
         return self
@@ -293,7 +299,7 @@ if __name__ == '__main__':
     data_folder_path = os.path.join(str(project_root()), 'MetaFEDOT\\data\\knowledge_base_time_series_0\\datasets\\')
     dataset_names = [name for name in os.listdir(data_folder_path)]
 
-    train, test = train_test_split(dataset_names, test_size=3)
+    train, test = train_test_split(dataset_names, test_size=3, random_state=42)
 
     train_datasets = {}
     for dataset in train:
@@ -302,10 +308,10 @@ if __name__ == '__main__':
     path_to_meta_data = os.path.join(str(project_root()),
                                      'MetaFEDOT\\data\\knowledge_base_time_series_0\\meta_features_ts.csv')
     dataloader = DataLoader_TS(train_datasets, path_to_meta_data=path_to_meta_data)
-    train_data, test_data, predict_input, meta_data = dataloader.get_data()
+    train_data, test_data, meta_data = dataloader.get_data(dataset_name='M4_W343')
 
     env = TimeSeriesPipelineEnvironment(render_mode='pipeline_plot', metadata_dim=125)
-    env.load_data(train_data, test_data, predict_input, meta_data)
+    env.load_data(train_data, test_data, meta_data)
     terminated = False
 
     total_reward = 0