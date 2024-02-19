Fix masking actions,update params, refactoring env for new rules and info

rl_core/agent/agent.py
@@ -72,11 +72,12 @@ class ActorCriticModel(nn.Module):
             nn.Linear(hidden_dims[-1], output_dim),
         ).to(self.device)
 
-    def forward(self, state):
+    def forward(self, state, mask):
         state = self.net(state.to(self.device))
 
         action_probs = self.actor_head(state)
-        dist = Categorical(action_probs)
+        masked_probs = mask * action_probs / torch.sum(mask * action_probs)
+        dist = Categorical(masked_probs)
         action = dist.sample()
 
         q_values = self.critic_head(state)

rl_core/agent/ppo.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.distributions import Categorical
 
 
@@ -33,9 +32,9 @@ class Buffer:
 class PPO(nn.Module):
     metadata = {'name': 'PPO'}
 
-    def __init__(self, state_dim, action_dim, hidden_dim: int = 128, gamma: float = 0.9, batch_size: int = 128,
-                 epsilon: float = 0.2, tau: float = 1.0,
-                 pi_lr: float = 3e-4, v_lr: float = 3e-4, epoch_n: int = 30, device: str = 'cpu'):
+    def __init__(self, state_dim, action_dim, hidden_dim: int = 128, gamma: float = 0.95, batch_size: int = 128,
+                 epsilon: float = 0.1, tau: float = 0.01,
+                 pi_lr: float = 1e-4, v_lr: float = 3e-4, epoch_n: int = 30, device: str = 'cpu'):
         super().__init__()
 
         self.state_dim = state_dim
@@ -139,9 +138,7 @@ class PPO(nn.Module):
                 self.v_optimizer.step()
                 self.v_optimizer.zero_grad()
 
-                kl_div = torch.mean((b_ratio - 1) - b_ratio).detach().cpu().numpy()
-
-        return pi_loss, v_loss, kl_div
+        return pi_loss, v_loss
 
     def append_to_buffer(self, s, a, r, terminated, mask):
         self.buffer.append(s, a, r, terminated, mask)

rl_core/ts_experiment.py
@@ -3,7 +3,6 @@ import os
 import numpy as np
 from sklearn.model_selection import train_test_split
 from torch.utils.tensorboard import SummaryWriter
-from tqdm import tqdm
 
 from meta_automl.utils import project_root
 from rl_core.agent.ppo import PPO
@@ -63,10 +62,10 @@ if __name__ == '__main__':
         episode_reward = 0
         episode_metric = 0
 
+        print('[', end='')
         while not done:
             action = agent.act(state, mask)
-
-            print(f'{action}:{env.get_action_code(action)}', end=' -> ')
+            print(f'{action}', end=', ')
 
             next_state, reward, terminated, truncated, info = env.step(action)
             mask = env.get_available_actions()
@@ -76,26 +75,28 @@ if __name__ == '__main__':
             agent.append_to_buffer(state, action, reward, done, mask)
             state = next_state
 
-        if info['pipeline'].nodes != []:
-            info['pipeline'].show()
+        print(']', end='')
+
+        # if info['pipeline'].nodes != []:
+        #     info['pipeline'].show()
 
         print(f'\n{info["pipeline"]}, metric {info["metric"]}, reward {episode_reward}')
+        print(f'{info["validation_rules"]}')
         print(f'-- Finishing {episode} episode --')
         print('')
         total_rewards.append(episode_reward)
         metric_value = info['metric'] if info['metric'] else 100000
         total_metrics.append(metric_value)
 
-        loss_1, loss_2, kl_div = agent.update()
+        loss_1, loss_2 = agent.update()
 
-        tb_writer.add_scalar('loss 1', loss_1, episode)
+        tb_writer.add_scalar('loss_1', loss_1, episode)
         tb_writer.add_scalar('loss_2', loss_2, episode)
-        tb_writer.add_scalar('kl_divergence', kl_div, episode)
 
         tb_writer.add_scalar('reward', episode_reward, episode)
         tb_writer.add_scalar('metric', metric_value, episode)
 
-        if episode % 100 == 0:
+        if episode % 50 == 0:
             agent.clear_buffer()
 
         if episode % 5 == 0: