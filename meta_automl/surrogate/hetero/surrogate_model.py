9092573b33b5b30c8fe66d047d2c5dd0d5aff8a4
Konstantin Chernyak <chernyakonstantin@gmail.com>
2024-03-07 17:11:25 +0300

    Add commented section with surrogate that concats two graphs to produce logit

--- meta_automl/surrogate/hetero/surrogate_model.py
+++ meta_automl/surrogate/hetero/surrogate_model.py
@@ -106,6 +106,94 @@ class HeteroPipelineRankingSurrogateModel(RankingPipelineSurrogateModel):
                 raise TypeError(f"Uknown type f{type(e)}")
         return res
 
+# # Model that concats embedding of two graphs and then produce logit
+# class HeteroPipelineRankingSurrogateModel(RankingPipelineSurrogateModel):
+#     def __init__(
+#         self,
+#         model_parameters: Dict[str, Any],
+#         lr: float = 1e-3,
+#         weight_decay: float = 1e-4,
+#         temperature: float = 10,
+#     ):
+#         LightningModule.__init__(self)
+
+#         if model_parameters["pipeline_encoder"]["type"] == "simple_graph_encoder":
+#             config = model_parameters["pipeline_encoder"]
+#             self.pipeline_encoder = SimpleGNNEncoder(**{k: v for k, v in config.items() if k != "type"})
+#         elif model_parameters["pipeline_encoder"]["type"] == "graph_transformer":
+#             config = model_parameters["pipeline_encoder"]
+#             self.pipeline_encoder = GraphTransformer(**{k: v for k, v in config.items() if k != "type"})
+
+#         self.final_model = nn.Sequential(
+#             nn.BatchNorm1d(self.pipeline_encoder.out_dim * 2),
+#             nn.Linear(self.pipeline_encoder.out_dim * 2, self.pipeline_encoder.out_dim),
+#             nn.ReLU(),
+#             nn.Linear(self.pipeline_encoder.out_dim, self.pipeline_encoder.out_dim),
+#             nn.ReLU(),
+#             nn.Linear(self.pipeline_encoder.out_dim, 1),
+#         )
+
+#         self.lr = lr
+#         self.weight_decay = weight_decay
+#         self.temperature = temperature
+
+#         # Migration to pytorch_lightning > 1.9.5
+#         self.validation_step_outputs = []
+#         self.test_step_outputs = []
+#         self.save_hyperparameters()  # TODO: is it required? We have config file.
+
+#         self.K_TOP = 3
+#         self.pipeline_extractor = FEDOTPipelineFeaturesExtractor(**model_parameters["pipeline_extractor"])
+#         self.node_embedder = build_node_embedder(model_parameters["node_embedder"])
+
+#     def training_step(
+#         self, batch: Tuple[HeterogeneousBatch, HeterogeneousBatch, torch.Tensor], *args, **kwargs
+#     ) -> Tensor:
+#         pipe1, pipe2, y = batch
+#         node_embedding1 = self.node_embedder(pipe1)
+#         node_embedding2 = self.node_embedder(pipe2)
+#         x_pipe1 = pipe1.to_pyg_batch(self.node_embedder.out_dim, node_embedding1)
+#         x_pipe2 = pipe2.to_pyg_batch(self.node_embedder.out_dim, node_embedding2)
+
+#         z_pipeline1 = self.pipeline_encoder(x_pipe1)
+#         z_pipeline2 = self.pipeline_encoder(x_pipe2)
+
+#         z_pipeline = torch.hstack([z_pipeline1, z_pipeline2])
+#         logit = self.final_model(z_pipeline)
+#         logit = torch.squeeze(logit)
+#         loss = nn.functional.binary_cross_entropy_with_logits(logit * self.temperature, y)
+#         self.log("train_loss", loss)
+#         return loss
+
+#     def validation_step(self, batch: Tuple[Tensor, Tensor, HeterogeneousBatch, Tensor], *args, **kwargs: Any) -> None:
+#         pass
+
+#     def test_step(self, batch: Tuple[Tensor, Tensor, HeterogeneousBatch, Tensor], *args, **kwargs: Any) -> None:
+#         pass
+
+#     def transfer_batch_to_device(self, batch: Any, device: torch.device, dataloader_idx: int) -> Any:
+#         res = []
+#         for e in batch:
+#             if isinstance(e, Tensor):
+#                 res.append(e.to(device))
+#             elif isinstance(e, HeterogeneousBatch):
+#                 e.batch = e.batch.to(device)
+#                 e.edge_index = e.edge_index.to(device)
+#                 e.node_idxes_per_type = {k: v.to(device) for k, v in e.node_idxes_per_type.items()}
+#                 e.hparams = {k: v.to(device) for k, v in e.hparams.items()}
+#                 e.encoded_type = {k: v.to(device) for k, v in e.encoded_type.items()}
+#                 res.append(e)
+#             else:
+#                 raise TypeError(f"Uknown type f{type(e)}")
+#         return res
+
+#     def on_validation_epoch_end(self) -> None:
+#         self.log("val_ndcg", -1)
+
+#     def on_test_epoch_end(self) -> None:
+#         self.log("val_ndcg", -1)
+
+# ---------------------------------------
 
 # class HeteroPipelineDatasetRankingSurrogateModelDetermenisticEmbedding(RankingPipelineDatasetSurrogateModel):
 #     def __init__(
