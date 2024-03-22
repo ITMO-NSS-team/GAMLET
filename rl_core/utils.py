4e66a94f38c10029c08bf370de197c0f024fd378
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2024-03-22 14:36:24 +0300

    Update experiment data and experience injection

--- rl_core/utils.py
+++ rl_core/utils.py
@@ -1,16 +1,43 @@
 import os
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
 from meta_automl.utils import project_root
 from rl_core.dataloader import DataLoader_TS
 
+OFFLINE_TRAJECTORIES = [
+    [2, 0],
+    [9, 0],
+    [11, 0],
+    [12, 0],
+    [13, 0],
+    [25, 1, 31, 0],
+    [25, 3, 31, 0],
+    [25, 4, 31, 0],
+    [25, 5, 31, 0],
+    [25, 6, 31, 0],
+    [25, 7, 31, 0],
+    [25, 8, 31, 0],
+    [25, 10, 31, 0],
+    [25, 9, 31, 0],
+    [25, 10, 31, 0],
+    [25, 14, 31, 0],
+]
 
-def define_data_for_experiment():
+
+def define_data_for_experiment(test_size: int = 3):
     data_folder_path = os.path.join(str(project_root()), 'MetaFEDOT\\data\\knowledge_base_time_series_0\\datasets\\')
     dataset_names = [name for name in os.listdir(data_folder_path)]
 
-    train, test = train_test_split(dataset_names, test_size=3)
+    temp = pd.read_csv('pipeline_validation_results.csv', index_col=0)
+    train = temp[temp['Topo Pipeline'].isna() == False]['Dataset'].to_list()
+    test = temp[temp['Topo Pipeline'].isna() == True]['Dataset'].to_list()
+
+    # if test_size:
+    #     train, test = train_test_split(dataset_names, test_size=3)
+    # else:
+    #     train, test = dataset_names, []
 
     train_datasets = {}
     for dataset in train:
@@ -23,6 +50,7 @@ def define_data_for_experiment():
     path_to_meta_data = os.path.join(str(project_root()),
                                      'MetaFEDOT\\data\\knowledge_base_time_series_0\\meta_features_ts.csv')
 
-    dataloader = DataLoader_TS(train_datasets, path_to_meta_data=path_to_meta_data)
+    dataloader_train = DataLoader_TS(train_datasets, path_to_meta_data=path_to_meta_data)
+    dataloader_test = DataLoader_TS(test_datasets, path_to_meta_data=path_to_meta_data)
 
-    return dataloader, train, test
+    return dataloader_train, dataloader_test, train, test,
