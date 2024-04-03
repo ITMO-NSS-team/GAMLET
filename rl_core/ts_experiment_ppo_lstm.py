65828769715d83cf009a9287e7df50406706c563
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2024-04-03 15:55:27 +0300

    Updates for experiments

--- rl_core/ts_experiment_ppo_lstm.py
+++ rl_core/ts_experiment_ppo_lstm.py
@@ -15,12 +15,10 @@ plt.ioff()
 sns.set(font_scale=0.5)
 
 
-if __name__ == '__main__':
-    number_of_nodes_in_pipeline = 3
-    n_episodes = 1000
-
+def run_experiment(n_episodes=2000, number_of_nodes_in_pipeline=3):
     dataloader, train_list, test_list = define_data_for_experiment()
-    env = TimeSeriesPipelineEnvironment(max_number_of_nodes=number_of_nodes_in_pipeline, render_mode='none', metadata_dim=126)
+    env = TimeSeriesPipelineEnvironment(max_number_of_nodes=number_of_nodes_in_pipeline, render_mode='none',
+                                        metadata_dim=126)
     state_dim, action_dim = env.state_dim, env.action_dim
     agent = RPPO(state_dim=state_dim, action_dim=action_dim, device='cuda')
 
@@ -29,6 +27,7 @@ if __name__ == '__main__':
     tb_writer = SummaryWriter(log_dir=log_dir)
     agent.create_log_report(log_dir)
     os.mkdir(f'{log_dir}/probs_heatmap')
+    os.mkdir(f'{log_dir}/weights')
 
     # -- Starting experiment --
 
@@ -36,7 +35,6 @@ if __name__ == '__main__':
     total_metrics = []
 
     period = 20
-    period_of_cleaning = 15
     period_of_heatmap = 100
 
     for episode in range(1, n_episodes + 1):
@@ -73,7 +71,7 @@ if __name__ == '__main__':
 
         print(f'\n{info["pipeline"]}, metric {info["metric"]}, reward {episode_reward}')
         print(f'{info["validation_rules"]}')
-        print(agent.buffer.get_size())
+        # print(agent.buffer.get_size())
 
         total_rewards.append(episode_reward)
         metric_value = info['metric'] if info['metric'] else 100000
@@ -83,8 +81,7 @@ if __name__ == '__main__':
         loss_1, loss_2, kld = agent.update()
         print(f'Q loss {loss_1} - V loss {loss_2} - KL Div {kld}')
 
-        if episode % period_of_cleaning == 0:
-            agent.clear_buffer()
+        agent.clear_buffer()
 
         print('-- Update tensorboard --')
         tb_writer.add_scalar('loss_1', loss_1, episode)
@@ -107,37 +104,39 @@ if __name__ == '__main__':
             message = f'Mean metric per last {period} episode'
             tb_writer.add_scalar(message, np.mean(total_metrics[-period:]), episode)
 
-        labels = (
-            np.asarray(
-                [np.around(value, 3) for value in probs_matrix.flatten()]
-            )
-        ).reshape(probs_matrix.shape)
-
-        actions_labels = [str(env.get_action_code(action)) for action in range(len(probs_matrix[:, 0]))]
-
-        fig = sns.heatmap(
-            probs_matrix,
-            annot=labels,
-            yticklabels=actions_labels,
-            linewidths=.05,
-            vmin=0.,
-            vmax=1.,
-            linecolor='gray',
-            cbar=False,
-            annot_kws={"fontsize": 8}
-        )
-        plt.xlabel('Step')
-        plt.ylabel('Action')
-        plt.savefig(f'{log_dir}/probs_heatmap/{episode}.png', format='png')
-
         if episode % period_of_heatmap == 0:
-            plt.show()
+            labels = (
+                np.asarray(
+                    [np.around(value, 3) for value in probs_matrix.flatten()]
+                )
+            ).reshape(probs_matrix.shape)
 
-        plt.close()
+            actions_labels = [str(env.get_action_code(action)) for action in range(len(probs_matrix[:, 0]))]
+
+            fig = sns.heatmap(
+                probs_matrix,
+                annot=labels,
+                yticklabels=actions_labels,
+                linewidths=.05,
+                vmin=0.,
+                vmax=1.,
+                linecolor='gray',
+                cbar=False,
+                annot_kws={"fontsize": 8}
+            )
+            plt.xlabel('Step')
+            plt.ylabel('Action')
+            plt.savefig(f'{log_dir}/probs_heatmap/{episode}.png', format='png')
+            plt.close()
 
         print(f'-- Finishing {episode} episode --\n')
 
     # -- Saving Agent ---
-    name = f'{env.metadata["name"]}_{state_dim}_{agent.metadata["name"]}_{agent.hidden_dim}_{n_episodes}'
-    path = f'{project_root()}/MetaFEDOT/rl_core/agent/pretrained/{name}'
+    name = f'{env.metadata["name"]}_{state_dim}_{number_of_nodes_in_pipeline}_{agent.metadata["name"]}_{agent.hidden_dim}_{n_episodes}'
+    path = f'{log_dir}/weights/{name}'
     agent.save(path)
+
+
+if __name__ == '__main__':
+    # for number_of_nodes in range(2, 11):
+    run_experiment(2000, 3)
