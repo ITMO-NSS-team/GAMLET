b4a2336ae4d13a6dbce4a01dbc0742d7efb2e431
Andrey Stebenkov <a.stebenkov75@yandex.ru>
2024-05-21 18:59:38 +0300

    Comment rl tests

--- rl_core/rl_test/test_environment.py
+++ rl_core/rl_test/test_environment.py
@@ -1,201 +1,201 @@
-import os.path
-
-import numpy as np
-import torch
-from fedot.core.pipelines.pipeline import Pipeline
-from fedot.core.repository.operation_types_repository import OperationTypesRepository
-from fedot.core.repository.tasks import TaskTypesEnum
-
-from gamlet.utils import project_root
-from rl_core.dataloader import DataLoader
-from rl_core.environments.ensemble import EnsemblePipelineGenerationEnvironment
-from rl_core.environments.linear import LinearPipelineGenerationEnvironment
-
-
-def test_linear_pipeline_environment():
-    primitives = OperationTypesRepository('all').suitable_operation(task_type=TaskTypesEnum.classification)
-
-    state_dim = 4
-    env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
-
-    initial_state = env.reset()
-
-    assert initial_state.numpy().shape == (state_dim,)
-
-    path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
-
-    datasets = {
-        'scoring': path,
-    }
-
-    dataloader = DataLoader(datasets)
-
-    train_data, val_data = dataloader.get_data()
-    env.load_data(train_data, val_data)
-
-    assert train_data == env.train_data
-    assert val_data == env.val_data
-
-    action = env.action_space.sample()
-
-    new_state, r, done, info = env.step(action)
-
-    assert initial_state.shape == new_state.shape
-    assert np.alltrue(initial_state.numpy() == new_state.numpy())
-    assert isinstance(r, float)
-    assert isinstance(done, bool)
-    assert isinstance(info, dict)
-
-
-def test_linear_pipeline_environment_pipeline_build():
-    primitives = OperationTypesRepository('all').suitable_operation(
-        task_type=TaskTypesEnum.classification)
-
-    state_dim = 2
-    env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
-
-    path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
-
-    datasets = {
-        'scoring': path,
-    }
-
-    dataloader = DataLoader(datasets)
-
-    train_data, val_data = dataloader.get_data()
-    env.load_data(train_data, val_data)
-
-    actions = [env.primitives.index(p) for p in ['rf', 'eop']]
-
-    for action in actions:
-        new_state, r, done, info = env.step(action)
-
-    assert isinstance(info['pipeline'], Pipeline)
-    assert isinstance(info['time_step'], int)
-    assert isinstance(info['metric_value'], float)
-    assert info['metric_value'] > 0.5
-
-
-def test_ensemble_pipeline_environment():
-    primitives = OperationTypesRepository('all').suitable_operation(task_type=TaskTypesEnum.classification)
-
-    state_dim = 4
-    env = EnsemblePipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
-
-    initial_state = env.reset()
-
-    assert initial_state.numpy().shape == (state_dim,)
-
-    path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
-
-    datasets = {
-        'scoring': path,
-    }
-
-    dataloader = DataLoader(datasets)
-
-    train_data, val_data = dataloader.get_data()
-    env.load_data(train_data, val_data)
-
-    assert train_data == env.train_data
-    assert val_data == env.val_data
-
-    action = env.action_space.sample()
-
-    new_state, r, done, info = env.step(action)
-
-    assert initial_state.shape == new_state.shape
-    assert np.alltrue(initial_state.numpy() == new_state.numpy())
-    assert isinstance(r, float)
-    assert isinstance(done, bool)
-    assert isinstance(info, dict)
-
-
-def test_ensemble_pipeline_environment_pipeline_build():
-    primitives = OperationTypesRepository('all').suitable_operation(
-        task_type=TaskTypesEnum.classification)
-
-    state_dim = 2
-    env = EnsemblePipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
-
-    path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
-
-    datasets = {
-        'scoring': path,
-    }
-
-    dataloader = DataLoader(datasets)
-
-    train_data, val_data = dataloader.get_data()
-    env.load_data(train_data, val_data)
-
-    actions = [env.primitives.index(p) for p in ['rf', 'scaling', 'knn', 'eop']]
-
-    for action in actions:
-        new_state, r, done, info = env.step(action)
-
-    assert isinstance(info['pipeline'], Pipeline)
-    assert isinstance(info['time_step'], int)
-    assert isinstance(info['metric_value'], float)
-    assert info['metric_value'] > 0.5
-
-
-def test_linear_pipeline_environment_pipeline_reset():
-    primitives = OperationTypesRepository('all').suitable_operation(
-        task_type=TaskTypesEnum.classification)
-
-    state_dim = 2
-    env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
-
-    path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
-
-    datasets = {
-        'scoring': path,
-    }
-
-    dataloader = DataLoader(datasets)
-
-    train_data, val_data = dataloader.get_data()
-    env.load_data(train_data, val_data)
-
-    actions = [env.primitives.index(p) for p in ['rf', 'dt', 'eop']]
-
-    for action in actions:
-        new_state, r, done, info = env.step(action)
-
-    env.reset()
-
-    assert torch.all(env.state == torch.tensor(np.array([0, 0]), dtype=torch.float64))
-
-    for action in actions:
-        new_state, r, done, info = env.step(action)
-
-    assert torch.all(new_state == torch.tensor(np.array([8, 2]), dtype=torch.float64))
-
-
-def test_linear_pipeline_environment_state():
-    primitives = OperationTypesRepository('all').suitable_operation(
-        task_type=TaskTypesEnum.classification)
-
-    state_dim = 4
-    env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
-
-    path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
-
-    datasets = {
-        'scoring': path,
-    }
-
-    dataloader = DataLoader(datasets)
-
-    train_data, val_data = dataloader.get_data()
-    env.load_data(train_data, val_data)
-
-    actions = [env.primitives.index(p) for p in ['knn', 'dt', 'scaling', 'rf']]
-    states = []
-
-    for action in actions:
-        new_state, r, done, info = env.step(action)
-        states.append(new_state.numpy())
-
-    assert np.unique(states, axis=1).shape[1] == len(states)
+# import os.path
+#
+# import numpy as np
+# import torch
+# from fedot.core.pipelines.pipeline import Pipeline
+# from fedot.core.repository.operation_types_repository import OperationTypesRepository
+# from fedot.core.repository.tasks import TaskTypesEnum
+#
+# from gamlet.utils import project_root
+# from rl_core.dataloader import DataLoader
+# from rl_core.environments.ensemble import EnsemblePipelineGenerationEnvironment
+# from rl_core.environments.linear import LinearPipelineGenerationEnvironment
+#
+#
+# def test_linear_pipeline_environment():
+#     primitives = OperationTypesRepository('all').suitable_operation(task_type=TaskTypesEnum.classification)
+#
+#     state_dim = 4
+#     env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
+#
+#     initial_state = env.reset()
+#
+#     assert initial_state.numpy().shape == (state_dim,)
+#
+#     path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
+#
+#     datasets = {
+#         'scoring': path,
+#     }
+#
+#     dataloader = DataLoader(datasets)
+#
+#     train_data, val_data = dataloader.get_data()
+#     env.load_data(train_data, val_data)
+#
+#     assert train_data == env.train_data
+#     assert val_data == env.val_data
+#
+#     action = env.action_space.sample()
+#
+#     new_state, r, done, info = env.step(action)
+#
+#     assert initial_state.shape == new_state.shape
+#     assert np.alltrue(initial_state.numpy() == new_state.numpy())
+#     assert isinstance(r, float)
+#     assert isinstance(done, bool)
+#     assert isinstance(info, dict)
+#
+#
+# def test_linear_pipeline_environment_pipeline_build():
+#     primitives = OperationTypesRepository('all').suitable_operation(
+#         task_type=TaskTypesEnum.classification)
+#
+#     state_dim = 2
+#     env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
+#
+#     path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
+#
+#     datasets = {
+#         'scoring': path,
+#     }
+#
+#     dataloader = DataLoader(datasets)
+#
+#     train_data, val_data = dataloader.get_data()
+#     env.load_data(train_data, val_data)
+#
+#     actions = [env.primitives.index(p) for p in ['rf', 'eop']]
+#
+#     for action in actions:
+#         new_state, r, done, info = env.step(action)
+#
+#     assert isinstance(info['pipeline'], Pipeline)
+#     assert isinstance(info['time_step'], int)
+#     assert isinstance(info['metric_value'], float)
+#     assert info['metric_value'] > 0.5
+#
+#
+# def test_ensemble_pipeline_environment():
+#     primitives = OperationTypesRepository('all').suitable_operation(task_type=TaskTypesEnum.classification)
+#
+#     state_dim = 4
+#     env = EnsemblePipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
+#
+#     initial_state = env.reset()
+#
+#     assert initial_state.numpy().shape == (state_dim,)
+#
+#     path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
+#
+#     datasets = {
+#         'scoring': path,
+#     }
+#
+#     dataloader = DataLoader(datasets)
+#
+#     train_data, val_data = dataloader.get_data()
+#     env.load_data(train_data, val_data)
+#
+#     assert train_data == env.train_data
+#     assert val_data == env.val_data
+#
+#     action = env.action_space.sample()
+#
+#     new_state, r, done, info = env.step(action)
+#
+#     assert initial_state.shape == new_state.shape
+#     assert np.alltrue(initial_state.numpy() == new_state.numpy())
+#     assert isinstance(r, float)
+#     assert isinstance(done, bool)
+#     assert isinstance(info, dict)
+#
+#
+# def test_ensemble_pipeline_environment_pipeline_build():
+#     primitives = OperationTypesRepository('all').suitable_operation(
+#         task_type=TaskTypesEnum.classification)
+#
+#     state_dim = 2
+#     env = EnsemblePipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
+#
+#     path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
+#
+#     datasets = {
+#         'scoring': path,
+#     }
+#
+#     dataloader = DataLoader(datasets)
+#
+#     train_data, val_data = dataloader.get_data()
+#     env.load_data(train_data, val_data)
+#
+#     actions = [env.primitives.index(p) for p in ['rf', 'scaling', 'knn', 'eop']]
+#
+#     for action in actions:
+#         new_state, r, done, info = env.step(action)
+#
+#     assert isinstance(info['pipeline'], Pipeline)
+#     assert isinstance(info['time_step'], int)
+#     assert isinstance(info['metric_value'], float)
+#     assert info['metric_value'] > 0.5
+#
+#
+# def test_linear_pipeline_environment_pipeline_reset():
+#     primitives = OperationTypesRepository('all').suitable_operation(
+#         task_type=TaskTypesEnum.classification)
+#
+#     state_dim = 2
+#     env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
+#
+#     path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
+#
+#     datasets = {
+#         'scoring': path,
+#     }
+#
+#     dataloader = DataLoader(datasets)
+#
+#     train_data, val_data = dataloader.get_data()
+#     env.load_data(train_data, val_data)
+#
+#     actions = [env.primitives.index(p) for p in ['rf', 'dt', 'eop']]
+#
+#     for action in actions:
+#         new_state, r, done, info = env.step(action)
+#
+#     env.reset()
+#
+#     assert torch.all(env.state == torch.tensor(np.array([0, 0]), dtype=torch.float64))
+#
+#     for action in actions:
+#         new_state, r, done, info = env.step(action)
+#
+#     assert torch.all(new_state == torch.tensor(np.array([8, 2]), dtype=torch.float64))
+#
+#
+# def test_linear_pipeline_environment_state():
+#     primitives = OperationTypesRepository('all').suitable_operation(
+#         task_type=TaskTypesEnum.classification)
+#
+#     state_dim = 4
+#     env = LinearPipelineGenerationEnvironment(state_dim=state_dim, primitives=primitives)
+#
+#     path = os.path.join(str(project_root()), 'MetaFEDOT/rl_core/data/scoring_train.csv')
+#
+#     datasets = {
+#         'scoring': path,
+#     }
+#
+#     dataloader = DataLoader(datasets)
+#
+#     train_data, val_data = dataloader.get_data()
+#     env.load_data(train_data, val_data)
+#
+#     actions = [env.primitives.index(p) for p in ['knn', 'dt', 'scaling', 'rf']]
+#     states = []
+#
+#     for action in actions:
+#         new_state, r, done, info = env.step(action)
+#         states.append(new_state.numpy())
+#
+#     assert np.unique(states, axis=1).shape[1] == len(states)
--- rl_core/rl_test/test_ts_env.py
+++ rl_core/rl_test/test_ts_env.py
@@ -1,148 +1,148 @@
-from itertools import permutations
-
-import numpy as np
-import pytest
-
-from rl_core.environments.time_series import TimeSeriesPipelineEnvironment
-from rl_core.rl_test.utils import get_time_series
-
-
-# {
-#       1: 'adareg',
-#       2: 'ar',
-#       3: 'dtreg',
-#       4: 'lasso',
-#       5: 'lgbmreg',
-#       6: 'linear',
-#       7: 'rfr',
-#       8: 'ridge',
-#       9: 'polyfit',
-#       10: 'sgdr',
-#       11: 'glm',
-#       12: 'ets',
-#       13: 'locf',
-#       14: 'ts_naive_average',
-#       15: 'svr',
-#       16: 'topological_features',
-#       17: 'scaling',
-#       18: 'normalization',
-#       19: 'pca',
-#       20: 'fast_ica',
-#       21: 'poly_features',
-#       22: 'ransac_lin_reg',
-#       23: 'ransac_non_lin_reg',
-#       24: 'isolation_forest_reg',
-#       25: 'lagged',
-#       26: 'sparse_lagged',
-#       27: 'smoothing',
-#       28: 'gaussian_filter',
-#       29: 'diff_filter',
-#       30: 'exog_ts'
-#  }
-
-
-@pytest.mark.parametrize('trajectory',
-                         [
-                             [2, 0], [9, 0], [11, 0], [12, 0], [13, 0], [14, 0],  # Single node pipelines
-                             [25, 1, 31, 0], [25, 3, 31, 0], [25, 4, 31, 0], [25, 5, 31, 0], [25, 6, 31, 0],
-                             # (1) lagged -> ..model..
-                             [25, 7, 31, 0], [25, 8, 31, 0], [25, 10, 31, 0], [25, 15, 31, 0],
-                             # (2) lagged -> ..model..
-                         ]
-                         )
-def test_correct_pipelines(trajectory):
-    train_data, test_data = get_time_series()
-
-    env = TimeSeriesPipelineEnvironment(metadata_dim=None)
-    env.load_data(train_data, test_data, meta=None)
-
-    total_reward = 0
-
-    env.reset()
-
-    for action in trajectory:
-        _, reward, _, _, info = env.step(action)
-        total_reward += reward
-
-    assert total_reward > 0
-    assert info['validation_rules']['valid'] is True
-
-
-@pytest.mark.parametrize('trajectory',
-                         [
-                             [1, 2, 3, 31, 41, 49, 0],  # Call pipeline.depth == -1
-                         ]
-                         )
-def test_uncorrect_pipelines(trajectory):
-    train_data, test_data = get_time_series()
-
-    env = TimeSeriesPipelineEnvironment(metadata_dim=None)
-    env.load_data(train_data, test_data, meta=None)
-
-    total_reward = 0
-
-    env.reset()
-
-    for action in trajectory:
-        _, reward, _, _, info = env.step(action)
-        total_reward += reward
-
-    assert total_reward < 0
-    assert info['validation_rules']['valid'] is False
-
-
-@pytest.mark.parametrize('max_number_of_nodes', [i for i in range(1, 25)])
-def test_max_number_of_actions_in_pipelines(max_number_of_nodes):
-    train_data, test_data = get_time_series()
-
-    env = TimeSeriesPipelineEnvironment(
-        max_number_of_nodes=max_number_of_nodes,
-        metadata_dim=0,
-        using_number_of_nodes=max_number_of_nodes
-    )
-    env.load_data(train_data, test_data, meta=None)
-    env.reset()
-
-    done = False
-    action_number = 0
-
-    while not done:
-        available_actions = env.get_available_actions()
-
-        if len(available_actions) == 1:
-            action = np.random.choice(available_actions)
-        else:
-            action = np.random.choice(available_actions)
-
-            while action == 0:
-                action = np.random.choice(available_actions)
-
-        _, _, terminated, truncated, info = env.step(action)
-        action_number += 1
-
-        done = terminated or truncated
-
-    assert action_number == env.max_number_of_actions
-
-
-@pytest.mark.parametrize('trajectory', [list(pair) for pair in permutations(range(1, 30), 2)])
-def test_correct_pairs_pipelines(trajectory: list):
-    train_data, test_data = get_time_series()
-
-    env = TimeSeriesPipelineEnvironment(metadata_dim=None, max_number_of_nodes=2)
-    env.load_data(train_data, test_data, meta=None)
-
-    trajectory = trajectory + [31, 0]
-
-    total_reward = 0
-    env.reset()
-
-    for action in trajectory:
-        _, reward, _, _, info = env.step(action)
-        total_reward += reward
-
-    assert total_reward > 0
-    assert info['validation_rules']['valid'] is True
-
-    if info['validation_rules']['valid'] is True:
-        print(info["pipeline"].structure)
+# from itertools import permutations
+#
+# import numpy as np
+# import pytest
+#
+# from rl_core.environments.time_series import TimeSeriesPipelineEnvironment
+# from rl_core.rl_test.utils import get_time_series
+#
+#
+# # {
+# #       1: 'adareg',
+# #       2: 'ar',
+# #       3: 'dtreg',
+# #       4: 'lasso',
+# #       5: 'lgbmreg',
+# #       6: 'linear',
+# #       7: 'rfr',
+# #       8: 'ridge',
+# #       9: 'polyfit',
+# #       10: 'sgdr',
+# #       11: 'glm',
+# #       12: 'ets',
+# #       13: 'locf',
+# #       14: 'ts_naive_average',
+# #       15: 'svr',
+# #       16: 'topological_features',
+# #       17: 'scaling',
+# #       18: 'normalization',
+# #       19: 'pca',
+# #       20: 'fast_ica',
+# #       21: 'poly_features',
+# #       22: 'ransac_lin_reg',
+# #       23: 'ransac_non_lin_reg',
+# #       24: 'isolation_forest_reg',
+# #       25: 'lagged',
+# #       26: 'sparse_lagged',
+# #       27: 'smoothing',
+# #       28: 'gaussian_filter',
+# #       29: 'diff_filter',
+# #       30: 'exog_ts'
+# #  }
+#
+#
+# @pytest.mark.parametrize('trajectory',
+#                          [
+#                              [2, 0], [9, 0], [11, 0], [12, 0], [13, 0], [14, 0],  # Single node pipelines
+#                              [25, 1, 31, 0], [25, 3, 31, 0], [25, 4, 31, 0], [25, 5, 31, 0], [25, 6, 31, 0],
+#                              # (1) lagged -> ..model..
+#                              [25, 7, 31, 0], [25, 8, 31, 0], [25, 10, 31, 0], [25, 15, 31, 0],
+#                              # (2) lagged -> ..model..
+#                          ]
+#                          )
+# def test_correct_pipelines(trajectory):
+#     train_data, test_data = get_time_series()
+#
+#     env = TimeSeriesPipelineEnvironment(metadata_dim=None)
+#     env.load_data(train_data, test_data, meta=None)
+#
+#     total_reward = 0
+#
+#     env.reset()
+#
+#     for action in trajectory:
+#         _, reward, _, _, info = env.step(action)
+#         total_reward += reward
+#
+#     assert total_reward > 0
+#     assert info['validation_rules']['valid'] is True
+#
+#
+# @pytest.mark.parametrize('trajectory',
+#                          [
+#                              [1, 2, 3, 31, 41, 49, 0],  # Call pipeline.depth == -1
+#                          ]
+#                          )
+# def test_uncorrect_pipelines(trajectory):
+#     train_data, test_data = get_time_series()
+#
+#     env = TimeSeriesPipelineEnvironment(metadata_dim=None)
+#     env.load_data(train_data, test_data, meta=None)
+#
+#     total_reward = 0
+#
+#     env.reset()
+#
+#     for action in trajectory:
+#         _, reward, _, _, info = env.step(action)
+#         total_reward += reward
+#
+#     assert total_reward < 0
+#     assert info['validation_rules']['valid'] is False
+#
+#
+# @pytest.mark.parametrize('max_number_of_nodes', [i for i in range(1, 25)])
+# def test_max_number_of_actions_in_pipelines(max_number_of_nodes):
+#     train_data, test_data = get_time_series()
+#
+#     env = TimeSeriesPipelineEnvironment(
+#         max_number_of_nodes=max_number_of_nodes,
+#         metadata_dim=0,
+#         using_number_of_nodes=max_number_of_nodes
+#     )
+#     env.load_data(train_data, test_data, meta=None)
+#     env.reset()
+#
+#     done = False
+#     action_number = 0
+#
+#     while not done:
+#         available_actions = env.get_available_actions()
+#
+#         if len(available_actions) == 1:
+#             action = np.random.choice(available_actions)
+#         else:
+#             action = np.random.choice(available_actions)
+#
+#             while action == 0:
+#                 action = np.random.choice(available_actions)
+#
+#         _, _, terminated, truncated, info = env.step(action)
+#         action_number += 1
+#
+#         done = terminated or truncated
+#
+#     assert action_number == env.max_number_of_actions
+#
+#
+# @pytest.mark.parametrize('trajectory', [list(pair) for pair in permutations(range(1, 30), 2)])
+# def test_correct_pairs_pipelines(trajectory: list):
+#     train_data, test_data = get_time_series()
+#
+#     env = TimeSeriesPipelineEnvironment(metadata_dim=None, max_number_of_nodes=2)
+#     env.load_data(train_data, test_data, meta=None)
+#
+#     trajectory = trajectory + [31, 0]
+#
+#     total_reward = 0
+#     env.reset()
+#
+#     for action in trajectory:
+#         _, reward, _, _, info = env.step(action)
+#         total_reward += reward
+#
+#     assert total_reward > 0
+#     assert info['validation_rules']['valid'] is True
+#
+#     if info['validation_rules']['valid'] is True:
+#         print(info["pipeline"].structure)
